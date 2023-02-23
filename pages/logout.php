89584804212bc00a419e841150f5b7319f65cfbe
Devashish Kapoor <devashishkpr@gmail.com>
2023-02-23 04:52:23 -0500

    add logout.php

---
+++ pages/logout.php
@@ -0,0 +1,30 @@
+<?php
+// start session
+session_start();
+// clear session
+$_SESSION = array();
+// end session
+session_destroy();
+if (isset($_POST["submit"])) {
+  // redirect
+  header("Location: stafflogin.php");
+  exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php $page_title = "Logout"; ?>
+    <?php include "includes/metadata.php" ?>
+  </head>
+  <body class="logout">
+    <?php include 'includes/header.php'; ?>
+    <section>
+      <form method="POST">
+        <h2>You have been logged out</h2>
+        <button type="submit" name="submit">Thank you!</button>
+      </form>
+    </section>
+    <?php include "includes/footer.php" ?>
+  </body>
+</html>
