56a3a9712928dbc32b0501934f0ebca31b890ce6
Devashish Kapoor <devashishkpr@gmail.com>
2023-03-08 15:58:25 -0500

    added internal navigation bar

--- includes/header.php
+++ includes/header.php
@@ -22,3 +22,9 @@
   </nav>
 </header>
 <main>
+<nav class="mainNav">
+  <?php if(isset($_SESSION['rollNumber'])) { ?>
+    <li><a href="mftipPlan.php">MFTIP Plans</a></li>
+    <li><a href="mftipPlanApprover.php">MFTIP Plan Approver</a></li>
+  <?php }?>
+  </nav>
