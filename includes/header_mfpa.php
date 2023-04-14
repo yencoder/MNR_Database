f73999c84e6e9569c557f1b7591c4f81e545f4ea
Devashish Kapoor <devashishkpr@gmail.com>
2023-04-14 10:49:42 -0400

    update includes

--- includes/header_mfpa.php
+++ includes/header_mfpa.php
@@ -3,19 +3,8 @@
   <nav>
     <ul>
       <li><a href="home.php">Home</a></li>
-      <?php
-        // session check for user information
-        if (!isset($_SESSION['rollNumber'])) { ?>
-          <li><a href="publicLogin.php">Public Login</a></li>
-      <?php
-        } elseif(!isset($_SESSION['rollNumber'])) { ?>
-          <li><a href="publicLogin.php">Public Login</a></li>
-      <?php 
-        } else { ?>
-          <li><a href="CLTIP_home.php">CLTIP</a></li>
-          <li><a href="mftipPlan.php">MFTIP</a></li>
-          <li><a href="logout.php">Logout</a></li>
-      <?php } ?>
+      <li><a href="logout.php">Logout</a></li>
+      
     </ul>
   </nav>
 </header>
